31abd84b153c9a5cfc7a1460013419b3f803c3c5
Cynthia Carol Marincovic Lopez <cynthiamarinkoviclopez@gmail.com>
2025-04-30 17:36:40 +0200

    Estructura básica de la landing page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
-import './App.css';
+import React from "react";
+import './App.css'; // Vinculamos el archivo de estilos
 
 function App() {
   return (
     <div className="App">
-      <div className="video-desktop">[VIDEO DESKTOP]</div>
-      <div className="video-mobile">[VIDEO MOBILE]</div>
-      <div className="video-smartwatch">[SMARTWATCH MODE]</div>
-      <section id="esfera">[Esfera viva (psicopompo)]</section>
-      <section id="portales">[Portafolio / Bio / Contacto]</section>
-      <section id="footer">[Texto final / Agencia de IA]</section>
+      <section id="intro"> {/* Sección de introducción */} </section>
+      <section id="esfera"> {/* Sección de la esfera psicopompo */} </section>
+      <section id="portales"> {/* Sección de portafolio / bio / contacto */} </section>
+      <section id="footer"> {/* Sección de pie de página */} </section>
     </div>
   );
 }
